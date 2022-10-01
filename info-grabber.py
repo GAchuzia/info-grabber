c0b497355138cde5c33bd2f2ac2a2a75d8594f7a
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2022-09-30 20:32:27 -0400

    Added text input for users

--- info-grabber.py
+++ info-grabber.py
@@ -153,26 +153,31 @@ def renameFile():
 
 # Gives the Building Data button its attributes
 buildingDataText = tk.StringVar()
-buildingDataBtn = tk.Button(root, textvariable = buildingDataText, command = buildingDataExtraction, font = "Calibri", bg = "#007940", fg = "white", height = 1, width = 12 )
+buildingDataBtn = tk.Button(root, textvariable = buildingDataText, command = buildingDataExtraction, font = "Calibri", bg = "#007940", fg = "white", height = 2, width = 30 )
 buildingDataText.set("Extract Building Data")
-buildingDataBtn.grid(column = 0, row =0, pady=6, padx=6, sticky= "nsew")
+buildingDataBtn.grid(column = 0, row =0, pady=6, padx=6)
 
 # Gives the Extract Images  button its attributes
 imageDataText = tk.StringVar()
-imageDataBtn = tk.Button(root, textvariable = imageDataText, command = imageDataExtraction,font = "Calibri", bg = "#007940", fg = "white", height = 1, width = 12 )
+imageDataBtn = tk.Button(root, textvariable = imageDataText, command = imageDataExtraction,font = "Calibri", bg = "#007940", fg = "white", height = 2, width = 62 )
 imageDataText.set("Extract Images")
-imageDataBtn.grid(column = 1, row = 0, pady=6, padx=6, sticky= "nsew")
+imageDataBtn.grid(column = 0, row = 1, pady=6, padx=4, columnspan=2)
 
 # Gives the Survey Data button its attributes
 surveyDataText = tk.StringVar()
-surveyDataBtn = tk.Button(root, textvariable = surveyDataText, command = surveyDataExtraction,font = "Calibri", bg = "#007940", fg = "white", height = 1, width = 12 )
+surveyDataBtn = tk.Button(root, textvariable = surveyDataText, command = surveyDataExtraction,font = "Calibri", bg = "#007940", fg = "white", height = 2, width = 30 )
 surveyDataText.set("Extract Survey Data")
-surveyDataBtn.grid(column = 0, row = 1, pady=6, padx=6, sticky= "nsew")
+surveyDataBtn.grid(column = 1, row = 0, pady=6, padx=6)
 
 # Gives the Rename File button its attributes
 renameFileText = tk.StringVar()
-renameFileBtn = tk.Button(root, textvariable = renameFileText, command = renameFile,font = "Calibri", bg = "#007940", fg = "white", height = 1, width = 12 )
+src_dir = tk.StringVar()
+rename_file_label = tk.Label(root, text="Rename File Here:", bg='white', pady=6, padx=6)
+rename_file_label.grid(column = 0, row = 4, pady=6, padx=6,  columnspan=2)
+input_path_area = tk.Entry(root, textvariable = src_dir, font = "Calibri",  width = 30)
+input_path_area.grid(column = 0, row = 3, pady=6, padx=6, sticky='nsew', columnspan=2)
+renameFileBtn = tk.Button(root, textvariable = renameFileText, command = renameFile,font = "Calibri", bg = "#007940", fg = "white", height = 1, width = 62 )
 renameFileText.set("Rename File(s)")
-renameFileBtn.grid(column = 1, row = 1, pady=6, padx=6, sticky= "nsew")
+renameFileBtn.grid(column = 0, row = 4, pady=6, padx=6,  columnspan=2)
 
 root.mainloop() #Also manages all tkinter components, do NOT put any code below this: it won't work
