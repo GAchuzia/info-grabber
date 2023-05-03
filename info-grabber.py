3af163aab2200711c226d94907261825f311da63
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-05-02 20:33:30 -0400

    Add function to get building data

--- info-grabber.py
+++ info-grabber.py
@@ -87,6 +87,49 @@ def get_building_data():
     info_box.insert("end", f"Get Building Data option selected\n")
     info_box.configure(state="disabled")
 
+    filetypes = [("DOCX Files", "*.docx")] #rename variable
+    selected_files = askopenfilenames(filetypes = filetypes)
+    
+    # docx to table
+    first_time = True # Make sure varibale is not declared more than once
+    for file in selected_files:
+        file_contents = docx2txt.process(file)
+        formatted_filecontents = file_contents.replace("\n"," ") #Turns docx file into formatted text
+
+        company_name = re.findall("(?<=Company Name\s\s)(.*)(?=Property Manager)|(?<=Company)(.*)(?=Contact Name)|(?<=Company:)(.*)(?=PREPARED BY:)",formatted_filecontents)
+        property_manager = re.findall("(?<=Property Manager\s\s)(.*)(?=Building Address)|(?<=Contact Name)(.*)(?=Building Name)|(?<=Contact:)(.*)(?=Building Name:)|(?<= Property Manager)(.*)(?=Address)",formatted_filecontents)
+        building_address = re.findall("(?<=Building Address\s\s)(.*)(?=City)|(?<= Building Address:)(.*)(?=,)|(?<=Building Address)(.*)(?=Roof Type:)|(?<=Building Address)(.*)(?=Job #)",formatted_filecontents)
+        file_city = re.findall("(?<=City\s\s)(.*)(?=Province)|(?<= ,)(.*)(?= Type of Roof: )",formatted_filecontents)
+        file_province = re.findall("(?<=Province\s\s)(.*)(?=Job #)",formatted_filecontents)
+        job_number = re.findall("(?<=Job #\s\s)(.*)(?=P.O.#)", formatted_filecontents)
+        po_number = re.findall("(?<=P.O.#\s\s)(.*)(?=GENERAL INFORMATION)",formatted_filecontents)
+
+        new_dict = {"Company Name": str(company_name).strip("[]").strip("()"),
+                    "Property Manager": str(property_manager).strip("[]").strip("()"),
+                    "Building Address": str(building_address).strip("[]").strip("()"),
+                    "City": str(file_city).strip("[]").strip("()"),                   
+                    "Province": str(file_province).strip("[]").strip("()"),
+                    "Job #": str(job_number).strip("[]").strip("()"),
+                    "P.O #": str(po_number).strip("[]").strip("()"),
+                    }
+
+        new_dict = {k: [v] for k, v in new_dict.items()} 
+        if first_time: # Same as first_time == True
+            first_time = False
+            df2export = pd.DataFrame.from_dict(new_dict) # Makes dataframe if its the first time
+        else: 
+            df2export = df2export.append(pd.DataFrame(new_dict), ignore_index = True) # Adds to the existing dataframe ()
+            df2export.dropna()
+
+    saving_path = filedialog.asksaveasfile(mode = 'w', defaultextension = ".csv")
+    df2export.to_csv(saving_path, index = False, line_terminator='\n')
+    saving_path.close()
+    print(df2export)
+
+    info_box.configure(state="normal")
+    info_box.insert("end", f"Building data extracted successfully\n")
+    info_box.configure(state="disabled")
+
 def get_survey_data():
     info_box.configure(state="normal")
     info_box.insert("end", f"Get Survey Data option selected\n")
